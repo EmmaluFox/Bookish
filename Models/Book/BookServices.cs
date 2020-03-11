5ced500368a9e4912f0dde8ec93bb4b0584fe08c
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-11 14:11:44 +0000

    Structure

--- Models/Book/BookServices.cs
+++ Models/Book/BookServices.cs
@@ -1,15 +1,29 @@
-﻿namespace Bookish.Models.Book
+﻿using System.Data;
+using System.Threading;
+
+namespace Bookish.Models.Book
 
 {
     using Dapper;
-    
-    public class Book
+
+    public class BookServices
     {
-        public int Id { get; set; }
-        public int AuthorId { get; set; }
-        public string Title { get; set; }
-        public string Isbn { get; set; }
-        
-        
+        public class Book
+        {
+            public int Id { get; set; }
+            public int AuthorId { get; set; }
+            public string Title { get; set; }
+            public string Isbn { get; set; }
+            public int OriginalPublicationYear { get; set; }
+
+        }
+        public static void GetBook()
+        {
+            
+        }
+
     }
+
 }
+
+
