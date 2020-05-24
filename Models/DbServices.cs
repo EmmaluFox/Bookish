3d012b6e12ac7d391a47b60889ed359a771cd9a0
Emmalu Fox <47330108+EmmaluFox@users.noreply.github.com>
2020-05-24 07:42:03 +0100

    Update DbServices.cs

--- Models/DbServices.cs
+++ Models/DbServices.cs
@@ -13,7 +13,7 @@ namespace Bookish.Models
             var server = "localhost";
             var database = "bookishdb";
             var uid = "EmmFox";
-            var password = "Astrid2014";
+            var password = "******";
             string serverConnection = "Server=" + server + ";" + "Database=" + 
                                       database + ";" + "Uid=" + uid + ";" + "Pwd=" + password + ";";
             using MySqlConnection connection = new MySqlConnection(serverConnection);
